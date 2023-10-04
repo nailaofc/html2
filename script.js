ddd986390dfa42fbf75f3f2862a3c07a5f13cfe1
Nabilaaaaaaaaaaaaa <akunabila070707@gmail.com>
2023-10-04 10:57:52 +0700

    search otw bagian produk unggulan

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-// toggle class active
+// Toggle Class Active Untuk Hamburger Menu
 const navbarNav = document.querySelector
 ('.navbar-nav');
 
-// ketika humberger menu diklik
+// Ketika Hamburger Menu Diklik
 document.querySelector('#hamburger-menu').
 onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle Class Active Untuk Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Klik Diluar Elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb =document.querySelector('#search-button');
 
 document.addEventListener('click' , function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 });
